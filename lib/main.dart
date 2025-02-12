cbc922c35d669747707884102b4cade910f794cc
sijiffs00 <sijiffs00@gmail.com>
2025-02-12 16:30:58 +0900

    Firebase 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:gpt_coin_trading/my_home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'firebase_options.dart';
+
+// 백그라운드 메시지를 처리하는 함수야
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("백그라운드 메시지 도착!: ${message.messageId}");
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting('ko_KR', null);
+  
+  // Firebase 초기화
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
+  // 백그라운드 메시지 핸들러 설정
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  
+  // 알림 권한 요청
+  final messaging = FirebaseMessaging.instance;
+  await messaging.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  
+  // FCM 토큰 가져오기
+  final token = await messaging.getToken();
+  print('FCM 토큰: $token');
   
   runApp(const MyApp());
 }
