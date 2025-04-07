0ce9989cd29bf57bc1dc73093c983cf5d40860f8
sijiffs00 <sijiffs00@gmail.com>
2025-04-07 15:16:06 +0900

    서버에있는 가상지갑 데이터 위젯 삭제

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -17,20 +17,11 @@ class _MyHomePageState extends State<MyHomePage> {
   // 거래 데이터
   List<Trade> trades = [];
 
-  // 지갑 정보 변수들
-  double returnRate = 0;
-  int seed = 0;
-  double btcBalance = 0;
-  double krwBalance = 0;
-  String lastUpdated = '';
-  bool isWalletLoading = true;
-
   @override
   void initState() {
     super.initState();
     // 앱 시작시 데이터 로드
     fetchData();
-    fetchWalletInfo(); // 지갑 정보도 함께 로드
   }
 
   // 매매기록들 가져오기
@@ -54,49 +45,12 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  // 지갑 정보 가져오기
-  Future<void> fetchWalletInfo() async {
-    try {
-      final response = await http.get(Uri.parse('$serverUrl/api/wallet'));
-
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
-
-        if (data['success'] == true) {
-          final wallet = data['wallet'];
-          setState(() {
-            returnRate = (wallet['return_rate'] ?? 0).toDouble();
-            seed = wallet['seed'] ?? 0;
-            btcBalance = (wallet['btc_balance'] ?? 0).toDouble();
-            krwBalance = (wallet['krw_balance'] ?? 0).toDouble();
-            lastUpdated = wallet['last_updated'] ?? '';
-            isWalletLoading = false;
-          });
-        }
-      }
-    } catch (e) {
-      print('지갑 정보 가져오기 실패: $e');
-      setState(() {
-        isWalletLoading = false;
-      });
-    }
-  }
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: TradesPage(
-              trades: trades,
-              returnRate: returnRate,
-              seed: seed,
-              btcBalance: btcBalance,
-              krwBalance: krwBalance,
-              lastUpdated: lastUpdated,
-              isWalletLoading: isWalletLoading,
-            ) 
-          
+        trades: trades,
+      ) 
     );
   }
 }
--- lib/pages/trades_page.dart
+++ lib/pages/trades_page.dart
@@ -1,30 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:gpt_coin_trading/widgets/wallet_info_widget.dart';
 import '../models/trade.dart';
 import '../widgets/trading_line_graph_widget.dart';
 import '../widgets/trading_records_list_widget.dart';
 import 'package:animated_background/animated_background.dart';
-class TradesPage extends StatefulWidget {
-  // trades 데이터를 받을 수 있도록 생성자 수정
-  final List<Trade> trades;
 
-  // 지갑 정보 변수들 추가
-  final double returnRate;
-  final int seed;
-  final double btcBalance;
-  final double krwBalance;
-  final String lastUpdated;
-  final bool isWalletLoading;
+class TradesPage extends StatefulWidget {
+  final List<Trade> trades;
 
   const TradesPage({
     super.key,
     required this.trades,
-    required this.returnRate,
-    required this.seed,
-    required this.btcBalance,
-    required this.krwBalance,
-    required this.lastUpdated,
-    required this.isWalletLoading,
   });
 
   @override
@@ -38,8 +23,6 @@ class _TradesPageState extends State<TradesPage> with TickerProviderStateMixin {
       backgroundColor: Colors.white,
       body: Stack(
         children: [
-        
-
           Container(
             width: MediaQuery.of(context).size.width,
             child: AnimatedBackground(
@@ -58,39 +41,14 @@ class _TradesPageState extends State<TradesPage> with TickerProviderStateMixin {
               vsync: this,)
           ),
 
-
-
-
           // 배경에 그래프 위젯 배치
           TradingLineGraphWidget(trades: widget.trades),
 
-
-          // const Text('공주라는걸\n잊지 않기_⭐️', style: TextStyle(fontSize: 50, fontFamily: '읏맨체', color: Color(0xccFF4AC1)),),
-
-          // 지갑 정보 위젯
-          Positioned(
-            bottom: 200,
-            left: 20,
-            right: 20,
-            child: WalletInfoWidget(
-              returnRate: widget.returnRate,
-              seed: widget.seed,
-              btcBalance: widget.btcBalance,
-              krwBalance: widget.krwBalance,
-              lastUpdated: widget.lastUpdated,
-              isLoading: widget.isWalletLoading,
-            ),
-          ),
-
           // DraggableScrollableSheet로 스크롤 가능한 위젯 구현
           DraggableScrollableSheet(
-            // 초기에는 화면의 절반만 차지
             initialChildSize: 0.5,
-            // 최소 크기는 화면의 20%
             minChildSize: 0.2,
-            // 최대 크기는 화면의 90%
             maxChildSize: 0.9,
-            // 스크롤 컨트롤러 생성
             builder: (BuildContext context, ScrollController scrollController) {
               return Container(
                 decoration:const BoxDecoration(
@@ -101,7 +59,7 @@ class _TradesPageState extends State<TradesPage> with TickerProviderStateMixin {
                       offset: Offset(0, -10),
                       blurRadius: 20,
                       spreadRadius: 0,
-                      color: Color(0x0F000000), // 6% 투명도의 검정색
+                      color: Color(0x0F000000),
                     ),
                   ],
                 ),
@@ -116,6 +74,4 @@ class _TradesPageState extends State<TradesPage> with TickerProviderStateMixin {
       ),
     );
   }
-
-  
 }
